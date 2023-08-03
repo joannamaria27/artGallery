f73cd3cd5358dde922429bd95c05bc06e560f0b2
Asia <joan.mk7@gmail.com>
2023-08-03 13:34:37 +0200

    feat: added CRUD product

---
+++ ArtGalleryWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using ArtGallery.DataAcess.Data;
+using ArtGallery.Models;
+using Microsoft.AspNetCore.Mvc;
+using ArtGallery.DataAcess.Repository.IRepository;
+
+namespace ArtGalleryWeb.Areas.Admin.Controllers
+{ 
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository context)
+        {
+            _productRepository = context;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _productRepository.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.Add(obj);
+                _productRepository.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product productFromDb = _productRepository.Get(u => u.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.Update(obj);
+                _productRepository.Save();
+                TempData["success"] = "Product edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product productFromDb = _productRepository.Get(u => u.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product obj = _productRepository.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _productRepository.Remove(obj);
+            _productRepository.Save();
+            TempData["success"] = "Product deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
--- ArtGalleryWeb/Program.cs
+++ ArtGalleryWeb/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
