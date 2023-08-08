a03ffea0a76e7bc47aa8daa6e5dee821c8916245
Asia <joan.mk7@gmail.com>
2023-08-08 18:57:45 +0200

    feat: added home controller

--- ArtGalleryWeb/Areas/Customer/Controllers/HomeController.cs
+++ ArtGalleryWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using ArtGallery.Models;
+﻿using ArtGallery.DataAcess.Repository.IRepository;
+using ArtGallery.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,18 +8,25 @@ namespace ArtGalleryWeb.Areas.Customer.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
+        private readonly IProductRepository _productRepository;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductRepository productRepository)
         {
             _logger = logger;
+            _productRepository = productRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _productRepository.GetAll(includePropertis: "Category").ToList();
+            return View(productList);
         }
-
+        public IActionResult Details(int productId)
+        {
+            Product product = _productRepository.Get(u=>u.Id== productId, includePropertis: "Category");
+            return View(product);
+        }
         public IActionResult Privacy()
         {
             return View();
